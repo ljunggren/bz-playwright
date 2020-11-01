4de016aca0ab22e45f4ae0eb78d1718ff25eb929
ljunggren <mljunggren@gmail.com>
2020-11-01 15:57:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,144 @@
+#! /usr/bin/env node
+// Device Descriptors https://github.com/GoogleChrome/puppeteer/blob/master/DeviceDescriptors.js
+
+
+(async () => {
+
+const { chromium } = require('playwright');
+const options = require('node-options');
+const { saveVideo } = require('playwright-video');
+const Service = require('./logService').Service;
+
+// Command defaults
+const opts = {
+  "verbose" : false,
+  "file": "",
+  "listscenarios":"",
+  "listsuite":"",
+  "device" : "",
+  "screenshot": false,
+  "token":"",
+  "userdatadir":"test",
+  "width":1280,
+  "height":1024,
+  "docker": false,
+  "gtimeout": "",
+  "notimeout": false,
+  "timeout": "",
+  "video": false
+}
+
+// Remove the first two arguments, which are the 'node' binary and the name
+// of your script.
+const result = options.parse(process.argv.slice(2), opts);
+const verbose = opts.verbose;
+const token = opts.token;
+const docker = opts.docker;
+let userdatadir = opts.userdatadir;
+const width = opts.width;
+const height = opts.height;
+const gtimeout=opts.gtimeout;
+const listscenarios=opts.listscenarios;
+const listsuite=opts.listsuite;
+const notimeout=opts.notimeout;
+const timeout= opts.timeout;
+const video = opts.video;
+let file = opts.file;
+
+if (result.errors || !result.args || result.args.length !== 1) {
+  console.log('USAGE: boozang [--token] [--docker] [--gtimeout] [--notimeout] [--verbose] [--userdatadir] [--listscenarios] [--listsuite] [--width] [--height] [--screenshot] [--file=report] [url]');
+  process.exit(2);
+}
+
+console.log("Running with following args");
+console.log(opts);
+console.log("Example: Use --verbose for verbose logging (boolean example). Use --width=800 to override default width (value example.)");
+
+
+  file = (docker ? "/var/boozang/" : "") + opts.file;
+
+  if (opts.userdatadir){
+    userdatadir = (docker ? "/var/boozang/userdatadir" : "") + (opts.userdatadir || "");
+    console.log("Setting userdatadir: " + userdatadir);
+  }
+  
+  const launchargs = [
+    '--disable-extensions-except=' + __dirname + '/bz-extension',
+    '--load-extension=' + __dirname + '/bz-extension',
+    '--ignore-certificate-errors',
+    '--no-sandbox',
+    `--window-size=${width},${height}`,
+    '--defaultViewport: null',
+    ];
+
+  const browser = await chromium.launchPersistentContext("bananas",{
+    headless: false,
+    args: launchargs,
+    launchType: "PERSISTENT"
+    /** ,
+    userDataDir: userdatadir,
+     */
+  });
+
+  function printStackTrace(app,err){
+    console.error(
+      "\n#######################################\n"
+    + app + " error: " + err.message
+    + "\n#######################################\n"
+    );   
+  }
+
+  function appPrintStackTrace(err){
+    printStackTrace("app",err);
+  }
+
+  function idePrintStackTrace(err){
+    printStackTrace("ide",err);
+  }
+
+  const page = await browser.newPage();
+  const capture = await saveVideo(page, 'recording.mp4', {followPopups:true});
+
+  //await capture.stop()
+  //await browser.close();
+
+
+  // Assign all log listeners
+  Service.logMonitor(page,notimeout,gtimeout,timeout,file, capture, browser)
+  if(listsuite||listscenarios){
+    Service.setBeginningFun(function(){
+      Service.insertFileTask(function(){
+        Service.result = 0;
+        Service.shutdown()
+      })
+      if(listsuite){
+        page.evaluate((v)=>{
+          $util.getTestsBySuite(v)
+        }, listsuite);
+      }else if(listscenarios){
+        page.evaluate((v)=>{
+          $util.getScenariosByTag(v)
+        }, JSON.parse(listscenarios));
+      }
+    })
+  }
+
+
+  let url = result.args[0];
+  if ((!opts.screenshot) && (!opts.listscenarios) && typeof (url) == 'string' && !url.endsWith("/run")) {
+    if (!url.endsWith("/")) {
+        url += "/"
+    }
+    url += "run"
+}
+  const response = await page.goto(url);
+
+  page.on("error", idePrintStackTrace);
+  page.on("pageerror", idePrintStackTrace);
+
+})()
+
+
+
+
+
