70fc42bb897125a9076ad09cccebd6df708d1be3
wensheng li <lws_home@hotmail.com>
2024-09-06 10:53:11 -0400

    master: d

--- ex-3/extension/content.js
+++ ex-3/extension/content.js
@@ -2,4 +2,4 @@ window.extensionContent=1;
 if((window.name||"").includes("bz-master")){
   document.documentElement.setAttribute('bz-id',chrome.runtime.id)
 }
-console.log("loading content.js")
+console.log("BZ-LOG: loading content.js")
